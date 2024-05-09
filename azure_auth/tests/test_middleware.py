2523b05f3526bc2988ac1d11ef74d396c56f8077
Armand Rego <armand@weirdsheeplabs.com>
2024-05-09 11:34:03 +0100

    Redirect to intended protected view after logging in #25.

--- azure_auth/tests/test_middleware.py
+++ azure_auth/tests/test_middleware.py
@@ -15,7 +15,8 @@ class TestAzureAuthMiddleware(TransactionTestCase):
         mocked_msal_app.return_value.acquire_token_silent.return_value = None
         resp = self.client.get(reverse("middleware_protected"))
         assert resp.status_code == HTTPStatus.FOUND
-        assert resp.url == reverse("azure_auth:login")  # type: ignore
+        assert resp.url == "/azure_auth/login"  # type: ignore
+        assert self.client.session.get("next") == "/middleware_protected/"
 
     def test_valid_token_unauthenticated_user(self, mocked_msal_app):
         # Not sure how this situation could arise but test anyway...
@@ -23,7 +24,8 @@ class TestAzureAuthMiddleware(TransactionTestCase):
         mocked_msal_app.return_value.acquire_token_silent.return_value = self.token  # type: ignore
         resp = self.client.get(reverse("middleware_protected"))
         assert resp.status_code == HTTPStatus.FOUND
-        assert resp.url == reverse("azure_auth:login")  # type: ignore
+        assert resp.url == "/azure_auth/login"  # type: ignore
+        assert self.client.session.get("next") == "/middleware_protected/"
 
     def test_valid_token_authenticated_user(self, mocked_msal_app):
         mocked_msal_app.return_value.acquire_token_silent.return_value = self.token  # type: ignore
--- azure_auth/views.py
+++ azure_auth/views.py
@@ -1,15 +1,15 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseForbidden, HttpResponseRedirect
 
 from .handlers import AuthHandler
 
 
-def azure_auth_login(request):
+def azure_auth_login(request: HttpRequest):
     return HttpResponseRedirect(AuthHandler(request).get_auth_uri())
 
 
-def azure_auth_logout(request):
+def azure_auth_logout(request: HttpRequest):
     # Auth handler has to be initialized before `logout()` to load the claims from the session
     auth_handler = AuthHandler(request)
 
@@ -17,11 +17,13 @@ def azure_auth_logout(request):
     return HttpResponseRedirect(auth_handler.get_logout_uri())
 
 
-def azure_auth_callback(request):
+def azure_auth_callback(request: HttpRequest):
     token = AuthHandler(request).get_token_from_flow()
     user = authenticate(request, token=token)
     if user:
+        # Get the `next` URL from the anonymous session before login
+        next = request.session.get("next", "")
         login(request, user)
     else:
         return HttpResponseForbidden("Invalid email for this app.")
-    return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+    return HttpResponseRedirect(next or settings.LOGIN_REDIRECT_URL)
